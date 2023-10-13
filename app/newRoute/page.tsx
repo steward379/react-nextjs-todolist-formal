ba9a27ed6414abaf4a5663fac660c398d8dc44c8
steward379 <steward379@gmail.com>
2023-10-13 21:15:21 +0800

    [FEAT] connect lines and dots on map markers

--- app/newRoute/page.tsx
+++ app/newRoute/page.tsx
@@ -3,13 +3,8 @@
 import GoogleMapComponent from '../../components/GoogleMapComponent';
 
 const NewRoutePage = () => {
-  const handleAddRoute = () => {
-    console.log('新增路線');
-  };
-
   return (
     <div>
-      <button onClick={handleAddRoute}>新增路線</button>
       <GoogleMapComponent />
     </div>
   );
--- components/GoogleMapComponent/index.tsx
+++ components/GoogleMapComponent/index.tsx
@@ -3,54 +3,71 @@ import { Loader } from '@googlemaps/js-api-loader';
 
 const GoogleMapComponent = () => {
   const mapRef = useRef(null);
-  let map;
-  const [currentMarker, setCurrentMarker] = useState(null);
+  const [map, setMap] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [isDrawingMode, setIsDrawingMode] = useState(false);
 
-  const addMarker = (location) => {
-    const marker = new window.google.maps.Marker({
-      position: location,
-      map: map,
-    });
-    setCurrentMarker(marker);
-  };
-
+  // 初始化地圖
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
       version: 'weekly',
     });
-
     loader.load().then(() => {
-      map = new window.google.maps.Map(mapRef.current, {
+      const initMap = new window.google.maps.Map(mapRef.current, {
         center: { lat: 25.0329694, lng: 121.5654177 },
         zoom: 12,
       });
-
-      map.addListener('click', (event) => {
-        addMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-      });
+      setMap(initMap);
     });
   }, []);
 
+  // 添加標記的函數
+  const addMarker = (event) => {
+    if (!isDrawingMode) return;
+
+    const marker = new window.google.maps.Marker({
+      position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+      map: map,
+    });
+
+    setMarkers(prevMarkers => {
+      const updatedMarkers = [...prevMarkers, marker];
+      if (updatedMarkers.length >= 2) {
+        drawLine(updatedMarkers);
+      }
+      return updatedMarkers;
+    });
+
+    setIsDrawingMode(false);
+  };
+
+  // 畫線的函數
+  const drawLine = (markerArray) => {
+    if (markerArray.length < 2) return;
+
+    const line = new window.google.maps.Polyline({
+      path: markerArray.map(marker => marker.getPosition()),
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+
+    line.setMap(map);
+  };
+
+  // 監聽地圖和繪圖模式
+  useEffect(() => {
+    if (map) {
+      map.addListener('click', addMarker);
+    }
+  }, [map, isDrawingMode]);
+
   return (
     <div>
+      <button onClick={() => setIsDrawingMode(true)}>新增路線</button>
       <div ref={mapRef} style={{ height: '80vh', width: '100%' }} />
-      {currentMarker && (
-        <div>
-          <h3>新增地點</h3>
-          <form>
-            <label>
-              名稱:
-              <input type="text" name="name" />
-            </label>
-            <label>
-              描述:
-              <input type="text" name="description" />
-            </label>
-            <input type="submit" value="完成" />
-          </form>
-        </div>
-      )}
     </div>
   );
 };
