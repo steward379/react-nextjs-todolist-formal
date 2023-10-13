36383ec37521df09c4e51e510ab25b36086bdf23
steward379 <steward379@gmail.com>
2023-10-13 23:02:53 +0800

    - [FEAT] allow people to write messages about the routes in the inputs.

--- components/GoogleMapComponent/index.tsx
+++ components/GoogleMapComponent/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 
 const GoogleMapComponent = () => {
@@ -6,8 +6,10 @@ const GoogleMapComponent = () => {
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
   const [isDrawingMode, setIsDrawingMode] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [line, setLine] = useState(null);
 
-  // 初始化地圖
+  // 初始化 Google 地圖
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
@@ -22,12 +24,13 @@ const GoogleMapComponent = () => {
     });
   }, []);
 
-  // 添加標記的函數
+  // 添加地圖上的標記
   const addMarker = (event) => {
-    if (!isDrawingMode) return;
+    if (!isDrawingMode || markers.length >= 3) return;
 
+    const newPoint = { lat: event.latLng.lat(), lng: event.latLng.lng() };
     const marker = new window.google.maps.Marker({
-      position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+      position: newPoint,
       map: map,
     });
 
@@ -42,32 +45,66 @@ const GoogleMapComponent = () => {
     setIsDrawingMode(false);
   };
 
-  // 畫線的函數
+  // 在標記之間畫線
   const drawLine = (markerArray) => {
-    if (markerArray.length < 2) return;
-
-    const line = new window.google.maps.Polyline({
+    if (line) {
+      line.setMap(null);
+    }
+    const newLine = new window.google.maps.Polyline({
       path: markerArray.map(marker => marker.getPosition()),
       geodesic: true,
       strokeColor: '#FF0000',
       strokeOpacity: 1.0,
       strokeWeight: 2,
     });
-
-    line.setMap(map);
+    newLine.setMap(map);
+    setLine(newLine);
   };
 
-  // 監聽地圖和繪圖模式
+  // 處理「完成」按鈕的點擊事件
+  const handleFinish = () => {
+    // 在這裡將 locations 儲存到資料庫
+    console.log("Locations saved:", locations);
+    setIsDrawingMode(false);
+    setMarkers([]);
+    if (line) {
+      line.setMap(null);
+    }
+    setLine(null);
+    setLocations([]);
+  };
+
   useEffect(() => {
     if (map) {
       map.addListener('click', addMarker);
     }
-  }, [map, isDrawingMode]);
+  }, [map, isDrawingMode, markers]);
 
   return (
     <div>
       <button onClick={() => setIsDrawingMode(true)}>新增路線</button>
       <div ref={mapRef} style={{ height: '80vh', width: '100%' }} />
+      {markers.length === 3 && (
+        <div>
+          <h3>請輸入地點和路線資訊</h3>
+          {Array.from({ length: 3 }, (_, index) => (
+            <div key={index}>
+              <h4>{`地點 ${index + 1}`}</h4>
+              <input type="text" placeholder="輸入地點名稱" onChange={(e) => {
+                const newLocations = [...locations];
+                newLocations[index] = { ...newLocations[index], name: e.target.value };
+                setLocations(newLocations);
+              }} />
+              <input type="text" placeholder="輸入地點描述" onChange={(e) => {
+                const newLocations = [...locations];
+                newLocations[index] = { ...newLocations[index], description: e.target.value };
+                setLocations(newLocations);
+              }} />
+            </div>
+          ))}
+          <button onClick={handleFinish}>路線完成</button>
+        </div>
+      )}
     </div>
   );
 };
